f606ab4c461ab9b6ab88cdfcfa2febeff314657a
joaomsfreis <joaomsfreis@gmail.com>
2019-10-27 15:47:15 -0300

    Aula 336

---
+++ bootstrap/projeto-galeria/src/index.js
@@ -0,0 +1,12 @@
+//Arquivos scss
+
+import './scss/index.scss'
+
+//Dependencias
+
+import 'jquery'
+import 'bootstrap'
+
+//Meus arquivos
+import './js/core/includes'
+import './js/plugins/cityButtons'
