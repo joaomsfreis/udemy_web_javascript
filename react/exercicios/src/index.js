7692b85cb4e342c26630c211852596cf6d6e8716
joaomsfreis <joaomsfreis@gmail.com>
2019-10-29 19:16:06 -0300

    Aula 345

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
 
 ReactDOM.render(
     <div>
-        <Pai sobrenome="Soares" nome="João" />
+        <Pai sobrenome="Soares" nome="João">
+            <Filho nome="Pedro"/>
+            <Filho nome="Paulo"/>
+            <Filho nome="Carla"/>
+        </Pai>
     </div>
     , document.getElementById('root'))
