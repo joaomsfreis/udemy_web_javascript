49c94a19bbe40d506e80b4dfe525b8ea893fb57f
João Marcos <joaomsfreis@gmail.com>
2019-12-15 17:02:11 -0300

    Aula 663

--- react/crud/frontend/src/main/App.jsx
+++ react/crud/frontend/src/main/App.jsx
@@ -2,16 +2,19 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
+import { HashRouter } from 'react-router-dom'
 
-import Logo from '../components/template/Logo' 
-import Main from '../components/template/Main' 
-import Nav from '../components/template/Nav' 
-import Footer from '../components/template/Footer' 
+import Routes from './Routes'
+import Logo from '../components/template/Logo'
+import Nav from '../components/template/Nav'
+import Footer from '../components/template/Footer'
 
 export default props =>
-    <div className="app">
-        <Logo />
-        <Nav />
-        <Main icon="home" title="Início" subtitle="Segundo Projeto do capítulo de React"/>
-        <Footer />
-    </div>
+    <HashRouter>
+        <div className="app">
+            <Logo />
+            <Nav />
+            <Routes />
+            <Footer />
+        </div>
+    </HashRouter>
